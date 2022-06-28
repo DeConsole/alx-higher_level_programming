cf6b4327f227a02a7ca79527f81c284a7759945b
DeConsole <dennisconsole@gmail.com>
2022-06-27 17:47:55 -0700

    creating 10-check_cycle.c

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+ * check_cycle - checks a cycle in a single link list in it
+ * @list: singly list
+ * Return: 0 if there is no cycle, 1 if there is a cycle
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *current;
+
+	if (list == NULL)
+		return (0);
+
+	current = list->next;
+
+	while (current != NULL)
+{
+	if (current == list)
+		return (1);
+	current = current->next;
+}
+	return (0);
+}
