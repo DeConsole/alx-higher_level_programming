7e02adf39c620411bdab5923864327106c946442
DeConsole <dennisconsole@gmail.com>
2022-06-29 17:39:36 -0700

    updating all files

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 for a in range(20, 46):
-    print('{:c}'.format(a), end='')
+    print('{:c}'.format(a), end=" ")
--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -2,4 +2,4 @@
 for a in range(20, 46):
     if a == 36 or a == 24:
         continue
-    print("{:c}".format(a), end='')
+    print("{:c}".format(a), end=" ")
